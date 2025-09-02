569f61b1bb4f3f37907856e8201ad9273f28e818
Mostafa Ibrahim <11410120200534@stud.cu.edu.eg>
2025-09-02 16:14:42 +0300

    Ready to use!

--- init.lua
+++ init.lua
@@ -8,3 +8,19 @@
 --
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+
+
+if vim.g.vscode then
+    -- VSCode extension
+    vim.opt.timeoutlen = 300
+    vim.g.mapleader = " "
+    -- Normal mode
+    vim.keymap.set('n', '<Space>', '<Cmd>call VSCodeNotify("whichkey.show")<CR>', { silent = true })
+    -- Visual mode
+    vim.keymap.set('v', '<Space>', '<Cmd>call VSCodeNotify("whichkey.show")<CR>', { silent = true })
+else
+    -- ordinary Neovim
+    vim.opt.timeoutlen = 300
+    vim.g.mapleader = " "
+end
